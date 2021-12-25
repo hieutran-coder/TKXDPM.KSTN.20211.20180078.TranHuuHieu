b3218a528455f2e11503a1dc827a00d8134bd3a3
Trần Hữu Hiếu <57060034+hieutran-coder@users.noreply.github.com>
2021-12-25 22:56:03 +0700

    Update NormalCalculateShippingFee.java

--- Programming/NormalCalculateShippingFee.java
+++ Programming/NormalCalculateShippingFee.java
@@ -1,5 +1,11 @@
 package controller;
 
 public class NormalCalculateShippingFee {
-
+    
+   public int normalCalculateShippingFee(Order order){
+        Random rand = new Random();
+        int fees = (int)( ( (rand.nextFloat()*10)/100 ) * order.getAmount() );
+        LOGGER.info("Order Amount: " + order.getAmount() + " -- Shipping Fees: " + fees);
+        return fees;
+    }
 }
