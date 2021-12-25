3a9d0a642b705a8b43236ca38937a1d43ab06e57
Trần Hữu Hiếu <57060034+hieutran-coder@users.noreply.github.com>
2021-12-25 22:49:47 +0700

    Update RushClaculateShippingFee.java

--- Programming/RushClaculateShippingFee.java
+++ Programming/RushClaculateShippingFee.java
@@ -1,5 +1,12 @@
 package controller;
 
-public class RushClaculateShippingFee {
-
+public class RushClaculateShippingFee extends CalculateShippingFee {
+   public int rushCalculateShippingFee(Order order) {
+        Random rand = new Random();
+        int fees = (int)( ( (rand.nextFloat()*10)/100 ) * order.getAmount());
+        int additionalFee = (int)( ( (rand.nextFloat()*10)/100 ) * (20 - Integer.parseInt(expectedDeliveryTime)));
+        fees += additionalFee;
+        LOGGER.info("Order Amount: " + order.getAmount() + " -- Shipping Fees: " + fees);
+        return fees;
+    }
 }
